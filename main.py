e44d10efe43762ad4c572577064b8905f86a2e69
AlekseyGostev <gostev.av@gmail.com>
2022-06-17 10:46:12 -0700

    completed

--- main.py
+++ main.py
@@ -35,16 +35,21 @@ def add_client(conn, first_name, last_name, email, phones=None, client_id=None):
     with conn.cursor() as cur:
         
         if client_id == None:
-            cur.execute("""
+            sql_request_text = """
             INSERT INTO client(first_name, last_name, email) VALUES(%s, %s, %s)
             RETURNING id;
-            """, (first_name, last_name, email))
+            """
+            params = (first_name, last_name, email)
         else:
-            cur.execute("""
+            sql_request_text = """
             INSERT INTO client(id, first_name, last_name, email) VALUES(%s, %s, %s, %s) 
             ON CONFLICT (id) DO UPDATE SET first_name = excluded.first_name, last_name = excluded.last_name, email = excluded.email
             RETURNING id;
-            """, (client_id, first_name, last_name, email))
+            """
+            params = (client_id, first_name, last_name, email)
+
+        
+        cur.execute(sql_request_text, params)
         cur_client_id = cur.fetchone()
 
         if phones != None:
@@ -59,19 +64,54 @@ def add_client(conn, first_name, last_name, email, phones=None, client_id=None):
     return
 
 
-def add_phone(conn, client_id, phone):
+def add_phone(conn, client_id, phone, no_commit=False):
     with conn.cursor() as cur:
         cur.execute("""
         INSERT INTO phones(client_id, number) VALUES(%s, %s)
         ON CONFLICT (client_id, number) DO NOTHING;
         """, (client_id, phone))
-        conn.commit()
+        if no_commit != True:
+            conn.commit()
 
     return
 
 
 def change_client(conn, client_id, first_name=None, last_name=None, email=None, phones=None):
-    pass
+    
+    with conn.cursor() as cur:
+
+        values_text = []    
+        values = []
+        if first_name != None:
+            values_text.append('first_name')
+            values.append(first_name)
+        if last_name != None:
+            values_text.append('last_name')
+            values.append(last_name)
+        if email != None:
+            values_text.append('email')
+            values.append(email)
+        
+        values.append(client_id)
+        
+        sql_request_text = f"""
+                UPDATE client SET {", ".join([x + " = %s" for x in values_text])}
+                WHERE id = %s
+                """
+        # print(sql_request_text)
+    
+        cur.execute(sql_request_text, values)
+
+        if phones != None:
+            cur.execute("""
+            DELETE FROM phones WHERE client_id = %s;
+            """, (client_id,))
+            for number in phones:
+                add_phone(conn=conn, client_id=client_id, phone=number, no_commit=True)
+            
+        conn.commit()    
+    
+    return
 
 
 def delete_phone(conn, client_id, phone):
@@ -90,7 +130,7 @@ def delete_client(conn, client_id):
         DELETE FROM phones WHERE client_id = %s;
         """, (client_id,))
         cur.execute("""
-        DELETE FROM client WHERE client_id = %s;
+        DELETE FROM client WHERE id = %s;
         """, (client_id,))
         conn.commit()
     
@@ -99,7 +139,32 @@ def delete_client(conn, client_id):
 
 def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
     
+    conditions = []
+    params = []
+    if first_name != None:
+        conditions.append('cl.first_name')
+        params.append(first_name)
+    if last_name != None:
+        conditions.append('cl.last_name')
+        params.append(last_name)
+    if email != None:
+        conditions.append('cl.email')
+        params.append(email)
+    if phone != None:
+        conditions.append('ph.number')
+        params.append(phone)
 
+    sql_request_text = f"""
+            SELECT * FROM client cl
+            LEFT JOIN phones ph ON cl.id = ph.client_id
+            WHERE {" AND ".join([x + " = %s" for x in conditions])}
+            """
+    # print(sql_request_text)
+
+    with conn.cursor() as cur:
+        cur.execute(sql_request_text, params)
+        print(cur.fetchall()) 
+    
     return
 
 
@@ -122,6 +187,7 @@ if __name__ == '__main__':
 
         # Функция, позволяющая изменить данные о клиенте
         change_client(conn, client_id=1, last_name='Sidorov', email='sidorovp@mail.ru', phones=['+79260000001'])
+        find_client(conn, last_name='Sidorov')
 
         # Функция, позволяющая удалить телефон для существующего клиента
         delete_phone(conn, client_id=2, phone='+79261112235')
